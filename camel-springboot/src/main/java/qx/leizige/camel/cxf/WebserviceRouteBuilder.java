5fccc744e427a819ed91fccd4a0f46165bf3cab6
= <qx_leizige@163.com>
2022-03-02 17:10:36 +0800

    添加netty-http依赖

--- camel-springboot/src/main/java/qx/leizige/camel/cxf/WebserviceRouteBuilder.java
+++ camel-springboot/src/main/java/qx/leizige/camel/cxf/WebserviceRouteBuilder.java
@@ -6,7 +6,7 @@ import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class WebserviceRouteBuilder extends RouteBuilder {
 
     private ProducerTemplate producerTemplate;
