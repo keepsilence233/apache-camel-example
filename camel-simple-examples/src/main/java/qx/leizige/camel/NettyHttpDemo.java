dc233edf430ffca8de650ed75a6b6a42c5f360ff
“leizige” <qx_leizige@163.com>
2022-09-13 14:03:33 +0800

    update pom.xml

--- camel-simple-examples/src/main/java/qx/leizige/camel/NettyHttpDemo.java
+++ camel-simple-examples/src/main/java/qx/leizige/camel/NettyHttpDemo.java
@@ -1,18 +1,27 @@
 package qx.leizige.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Header;
 import org.apache.camel.builder.RouteBuilder;
 import qx.leizige.camel.util.CamelUtil;
 
 public class NettyHttpDemo {
 
     public static void main(String[] args) throws Exception {
+        String json = "{\"key\":\"123123\"}";
         //netty-http:http://0.0.0.0:8080[?options]
         CamelContext context = CamelUtil.getCamelContext(new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                from("timer:check?period=5m")
-                        .to("netty-http:http://106.14.149.149:8380/caculate/actuator/info")
+                from("netty-http:localhost:9890/test")
+                        .setBody(simple(json,String.class))
+                        .process(exchange -> {
+                            System.out.println(exchange.getIn().getBody());
+                        })
+//                        .filter().xpath("[?(<$.key == '123123'>)]")
+                        .filter().method(MyBean.class,"isGoldCustomer")
+                        .to("netty-http:http://101.132.24.133:8803/middle-platform-adapter/actuator")
                         .process((exchange -> {
                             String body = exchange.getIn().getBody(String.class);
                             System.out.println("body = " + body);
@@ -27,4 +36,12 @@ public class NettyHttpDemo {
 
     }
 
+    public static class MyBean {
+        public boolean isGoldCustomer(Exchange exchange) {
+            String body = exchange.getIn().getBody(String.class);
+            System.out.println(body);
+            exchange.getMessage().setBody("{\"message\":\"success\"}");
+            return false;
+        }
+    }
 }
