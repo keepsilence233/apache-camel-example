4a66c45abab0f83403f3534284f03bf01c1d074c
leizige <leit.tl@bitsun-inc.com>
2023-02-12 16:51:03 +0800

    FirstTest update

--- camel-springboot/src/test/java/org/apache/camel/test/FirstTest.java
+++ camel-springboot/src/test/java/org/apache/camel/test/FirstTest.java
@@ -7,7 +7,20 @@ import org.junit.Test;
 
 import java.io.File;
 
+/**
+ * 单元测试来测试将文件从一个目录复制到另一个目录的Camel路由
+ */
 public class FirstTest extends BaseCamelTest {
+
+    /**
+     * 确保起始目录为空
+     */
+    @Override
+    public void setUp() throws Exception {
+        deleteDirectory("/Users/chinese.youth/temp/A");
+        deleteDirectory("/Users/chinese.youth/temp/B");
+        super.setUp();
+    }
 
     @Override
     protected RoutesBuilder createRouteBuilder() throws Exception {
@@ -21,10 +34,15 @@ public class FirstTest extends BaseCamelTest {
     }
 
     @Test
-    public void testMove() throws InterruptedException {
+    public void testMoveFile() throws InterruptedException {
         template.sendBodyAndHeader("file:/Users/chinese.youth/temp/A", "Hello World", Exchange.FILE_NAME, "hello.txt");
+
         Thread.sleep(2000);
+
         File target = new File("/Users/chinese.youth/temp/B/hello.txt");
         assertTrue("File not moved", target.exists());
+
+        String content = context.getTypeConverter().convertTo(String.class, target);
+        assertEquals("Hello World", content);
     }
 }
